7cd69431e11f46788053a7d5980334fb683247cd
luisOrtegas <105408367+luisOrtegas@users.noreply.github.com>
2026-08-01 02:30:16 -0600

    Enable demo seeding in local environment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if (app()->environment('local')) {
+            $this->call(DemoDataSeeder::class);
+        }
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
