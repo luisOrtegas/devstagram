9cfa5927a785b92750d591f443a38a10fb47414d
luisOrtegas <105408367+luisOrtegas@users.noreply.github.com>
2026-08-02 00:14:31 -0600

    Remove generated titles from image alternatives

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,7 @@
             <div class="flex min-h-80 items-center justify-center bg-gray-950">
                 <img
                     src="{{ asset('uploads/' . $post->imagen) }}"
-                    alt="Imagen de la publicación {{ $post->titulo }}"
+                    alt="{{ \Illuminate\Support\Str::startsWith($post->titulo, 'Galería Devstagram') ? 'Imagen publicada en Devstagram' : 'Imagen de la publicación ' . $post->titulo }}"
                     class="max-h-[70vh] w-full object-contain"
                 >
             </div>
