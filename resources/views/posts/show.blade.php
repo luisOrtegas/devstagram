7e5d332ff078c391a52bf2984a9a839b4cd7e32e
luisOrtegas <105408367+luisOrtegas@users.noreply.github.com>
2026-08-02 00:00:31 -0600

    Hide gallery descriptions and show delete controls

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -31,7 +31,7 @@
 
                 <h1 class="mt-5 break-words text-2xl font-black text-gray-900">{{ $post->titulo }}</h1>
 
-                @if ($post->descripcion && !\Illuminate\Support\Str::contains(\Illuminate\Support\Str::lower($post->descripcion), 'galería local de devstagram'))
+                @if ($post->descripcion && !\Illuminate\Support\Str::startsWith($post->titulo, 'Galería Devstagram'))
                     <p class="mt-3 whitespace-pre-line break-words leading-7 text-gray-700">{{ $post->descripcion }}</p>
                 @endif
 
