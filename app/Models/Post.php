224db9486b93c296a663663c80ef0da6176b8762
luisOrtegas <105408367+luisOrtegas@users.noreply.github.com>
2026-08-01 23:24:14 -0600

    Improve publication and comment layout

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,12 +18,12 @@ class Post extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class)->select(['name', 'username']);
+        return $this->belongsTo(User::class);
     }
 
     public function comentarios()
     {
-        return $this->hasMany(Comentario::class);
+        return $this->hasMany(Comentario::class)->latest();
     }
 
     public function likes()
@@ -36,4 +36,3 @@ class Post extends Model
         return $this->likes->contains('user_id', $user->id);
     }
 }
-
