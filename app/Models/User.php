400f74f094793611e6f6f3ef9a25b1f28d0826dc
luisOrtegas <105408367+luisOrtegas@users.noreply.github.com>
2026-08-03 20:58:45 -0600

    Separate profile details and publications

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'username'
+        'username',
+        'telefono',
+        'direccion',
+        'biografia'
     ];
 
     /**
