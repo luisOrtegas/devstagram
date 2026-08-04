be410d042790c62677101cb299b7e3fa69951353
luisOrtegas <105408367+luisOrtegas@users.noreply.github.com>
2026-08-03 19:28:01 -0600

    Add database notifications

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -5,23 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Comentario;
+use App\Notifications\NewCommentNotification;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Request $request, User $user, Post $post)
     {
+        abort_unless($post->user_id === $user->id, 404);
+
         //Validar
         $this->validate($request, [
             'comentario' => 'required|max:255'
         ]);
         
         //Almacenar el resultado
-        Comentario::create([
+        $comentario = Comentario::create([
             'user_id' => auth()->user()->id,
             'post_id' => $post->id,
             'comentario' => $request->comentario
         ]);
+
+        if ($post->user_id !== auth()->id()) {
+            $post->user->notify(
+                new NewCommentNotification($post, $comentario, auth()->user())
+            );
+        }
 
         //Imprimir un mensaje
         return back()->with('mensaje', 'Comentario Realizado Correctamente');
